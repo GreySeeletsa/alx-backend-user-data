a414770ec50f18c9747374fc80385af4f0561824
GreySeeletsa <seeletsagrey@gmail.com>
2024-02-22 16:51:48 +0000

    Created a SQLAlchemy model named User for a database table named users

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """User Module"""
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+
 Base = declarative_base()
 
-"""A SQLAlchemy model named User with id, email, hashed_password, session_id
-and reset_token attributes"""
+"""A SQLAlchemy model named User with id, email, hashed_password,
+session_id and reset_token attributes"""
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String)
-    reset_token = Column(String)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
